b7eae3b70c50c0f522ee339cd595c7c23abbe3e2
Ahmad Raza <dev@aiderivatives.co>
2023-02-26 20:18:34 +0500

    updates

--- components/Board/List.js
+++ components/Board/List.js
@@ -2,10 +2,14 @@ import Image from "next/image";
 import styles from "../../styles/Board.module.css";
 
 import dynamic from 'next/dynamic';
+import { useState } from "react";
 const Card = dynamic(() => import('./Card'), {
   ssr: false,
 })
 const List = ({name,card}) => {
+
+  const [toggle,settoggle] = useState(card==2?1:0);
+
   return (
     <div className={styles.list}>
       <div className="flex justify-between" style={{marginBottom:8}} >
@@ -31,11 +35,36 @@ const List = ({name,card}) => {
           
           
         </div>
+        {toggle==0&&
         <div className="flex justify-center">
-        <div className={styles.addcard}>
+        <div className={styles.addcard} onClick={()=>settoggle(!toggle)}>
           <Image src="/Plus2.svg" height={22} width={22} style={{margin:"0px 5px 0px 9px"}}></Image>Add Card
         </div> 
         </div>
+        }
+        {toggle==1&&
+        <div className={styles.showcard}>
+        <div className={styles.blueline}></div>
+        <div className="flex justify-between"><h2 className={styles.cardtitle}>Card title</h2>
+          <button className="btn"><img src="/Close.svg" onClick={()=>settoggle(!toggle)} /></button>
+        </div>
+        <textarea className={styles.mytextarea} placeholder="Enter card name"></textarea>
+        <div className="flex justify-between" style={{marginTop:4}}>
+          <select className={styles.selectproject}>
+            <option>Contact RM</option>
+            <option>Agent Book</option>
+            <option>W4RTeam</option>
+            <option>LeadBank</option>
+          </select>
+          <button className={styles.addbtn} onClick={()=>settoggle(!toggle)}>
+            Add
+          </button>
+        </div>
+
+        </div>
+
+      }
+        
      </div>
   );
 };
--- components/Sidebar/Header.js
+++ components/Sidebar/Header.js
@@ -7,7 +7,7 @@ const Sidebar = () =>{
 
     return (
         <div className={styles.header}>
-            <div className="flex justify-center items-center"  style={{columnGap:8,position: "relative",left: "15px"}}>
+            <div className="flex justify-center items-center right"  style={{columnGap:8,position: "relative",left: "15px"}}>
             <Image src="/pink.png" width="18" height="18" alt="pink"></Image> 
             <h1 className={styles.name}>Zaan Corp</h1>
             <Image className={styles.downarrow} src="/downarrow.svg" width="12" height="12" alt="downarrow" ></Image>
